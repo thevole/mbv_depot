cdb738a60046a64644c0190ff6c841202abb9e8f
Martin Volerich <martin@volerich.com>
2017-07-22 17:46:19 -0700

    Smarter cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,11 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+
+  def invalid_cart
+    logger.error "Attempt to access an invalid cart #{params[:id]}"
+    redirect_to store_index_url, notice: 'Invalid cart'
+  end
 
   # GET /carts
   # GET /carts.json
@@ -54,9 +60,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_index_url, notice: 'Your cart is currently empty.' }
       format.json { head :no_content }
     end
   end
